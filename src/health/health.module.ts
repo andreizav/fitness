89c27d277d68a36029bc88903c01766d9f10c59b
Andrey Zavoznenko <andreyz@corp.wapdwap.com>
2018-05-29 15:55:21 +0300

    continue

--- src/health/health.module.ts
+++ src/health/health.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { AuthGuard } from '../auth/shared/guards/auth.guard';
+
 export const ROUTES: Routes = [
-  { path: 'schedule', loadChildren: './schedule/schedule.module#ScheduleModule' },
-  { path: 'meals', loadChildren: './meals/meals.module#MealsModule' },
-  { path: 'workouts', loadChildren: './workouts/workouts.module#WorkoutsModule' }
+  { path: 'schedule', canActivate: [AuthGuard], loadChildren: './schedule/schedule.module#ScheduleModule' },
+  { path: 'meals', canActivate: [AuthGuard], loadChildren: './meals/meals.module#MealsModule' },
+  { path: 'workouts', canActivate: [AuthGuard], loadChildren: './workouts/workouts.module#WorkoutsModule' }
 ];
 
 @NgModule({
@@ -12,4 +14,4 @@ export const ROUTES: Routes = [
     RouterModule.forChild(ROUTES)
   ]
 })
-export class HealthModule {}
+export class HealthModule { }
